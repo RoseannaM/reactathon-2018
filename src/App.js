d6ffbe33444ca92b4f2455e132b6cf15310e8300
Stephen Bennett <sbennett@atlassian.com>
2018-03-24 15:19:21 -0700

    Event page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { Page1 } from "./pages/page1";
 import { Page2 } from "./pages/page2";
 import { EventStart } from "./pages/event-start";
-import { Event } from "./pages/event";
+import { EventPage } from "./pages/event-page";
 import { Landing } from "./pages/landing";
 import { Login } from "./pages/login";
 import { EventListPage } from "./pages/event-list-page";
@@ -25,9 +25,9 @@ class App extends Component {
             <Route path="/page1" component={Page1} />
             <Route path="/page2" component={Page2} />
             <Route path="/event-start" component={EventStart} />
-            <Route path="/event" component={Event} />
             <Route path="/login" component={Login} />
             <Route path="/event-list" component={EventListPage} />
+            <Route path="/event/:id" component={EventPage} />
           </div>
         </BrowserRouter>
       </ApolloProvider>
---
+++ src/pages/event-page.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import PageLayout from "../components/page-layout";
+import { compose, graphql } from "react-apollo";
+import eventQuery from "./event.query.graphql";
+import EventCardList from "../components/event-card-list";
+import EventCard from "../components/event-card";
+import withLoadingSpinner from "../components/with-loading-spinner";
+
+class EventPageView extends Component {
+  render() {
+    return <PageLayout title={this.props.data.event.title} />;
+  }
+}
+
+export const EventPage = compose(
+  graphql(eventQuery, {
+    options: props => ({
+      variables: {
+        id: props.match.id
+      }
+    })
+  }),
+  withLoadingSpinner
+)(EventPageView);
