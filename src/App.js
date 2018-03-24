0222c8b4fdc00a653e31947f54870ac51bd946d3
Joshua Nelson <jonelson@atlassian.com>
2018-03-24 12:25:34 -0700

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from "react";
 
-import Button from "material-ui/Button";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Page1 } from "./pages/page1";
 import { Page2 } from "./pages/page2";
+import { EventStart } from "./pages/EventStart";
+import { Landing } from "./pages/Landing";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Link to="/page1">
-            <Button variant="raised" color="primary">
-              Page 1
-            </Button>
-          </Link>
-          <Link to="/page2">
-            <Button variant="raised" color="primary">
-              Page 2
-            </Button>
-          </Link>
-
+          <Route path="/" exact component={Landing} />
           <Route path="/page1" component={Page1} />
           <Route path="/page2" component={Page2} />
+          <Route path="/event-start" component={EventStart} />
         </div>
       </BrowserRouter>
     );
---
+++ src/pages/EventStart.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Black = styled.div`background: black;`;
+
+export class EventStart extends Component {
+  render() {
+    return <Black>Page 1</Black>;
+  }
+}
