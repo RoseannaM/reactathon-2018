f262db44a4af9b10c1de70e968c01b571a6fef39
Joshua Nelson <jonelson@atlassian.com>
2018-03-24 09:39:19 -0700

    Add react router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Button from "material-ui/Button";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Page1 } from "./pages/page1";
+import { Page2 } from "./pages/page2";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button variant="raised" color="primary">
-          Hello World
-        </Button>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Link to="/page1">
+            <Button variant="raised" color="primary">
+              Page 1
+            </Button>
+          </Link>
+          <Link to="/page2">
+            <Button variant="raised" color="primary">
+              Page 2
+            </Button>
+          </Link>
+
+          <Route path="/page1" component={Page1} />
+          <Route path="/page2" component={Page2} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/pages/page1.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class Page1 extends Component {
+  render() {
+    return <p>Page 1</p>;
+  }
+}
