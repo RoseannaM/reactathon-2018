6f7494d1329153d2534a6ee5b0aa19a4b39840d1
Stephen Bennett <sbennett@atlassian.com>
2018-03-24 12:42:59 -0700

    GraphQL!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
+import { ApolloProvider } from "react-apollo";
+import { client } from "./apollo-client";
 
 import Button from "material-ui/Button";
 import { BrowserRouter, Route, Link } from "react-router-dom";
@@ -9,23 +11,25 @@ import { Page2 } from "./pages/page2";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Link to="/page1">
-            <Button variant="raised" color="primary">
-              Page 1
-            </Button>
-          </Link>
-          <Link to="/page2">
-            <Button variant="raised" color="primary">
-              Page 2
-            </Button>
-          </Link>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <div className="App">
+            <Link to="/page1">
+              <Button variant="raised" color="primary">
+                Page 1
+              </Button>
+            </Link>
+            <Link to="/page2">
+              <Button variant="raised" color="primary">
+                Page 2
+              </Button>
+            </Link>
 
-          <Route path="/page1" component={Page1} />
-          <Route path="/page2" component={Page2} />
-        </div>
-      </BrowserRouter>
+            <Route path="/page1" component={Page1} />
+            <Route path="/page2" component={Page2} />
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
     );
   }
 }
