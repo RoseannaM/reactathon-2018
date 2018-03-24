b4bd3fbda0b3a3610c76b11fc5b58d62bbace681
Joshua Nelson <jonelson@atlassian.com>
2018-03-24 09:42:42 -0700

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 
 import Button from "material-ui/Button";
 import { BrowserRouter, Route, Link } from "react-router-dom";
--- src/pages/page1.js
+++ src/pages/page1.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Fancy = styled.div`color: red;`;
 
 export class Page1 extends Component {
   render() {
-    return <p>Page 1</p>;
+    return <Fancy>Page 1</Fancy>;
   }
 }
