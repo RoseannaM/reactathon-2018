79cc1fb78eba5d24296e4500246347c5b317720d
Joshua Nelson <jonelson@atlassian.com>
2018-03-25 14:11:53 -0700

    Loading

--- src/components/with-loading-spinner.js
+++ src/components/with-loading-spinner.js
@@ -1,12 +1,70 @@
 import React from "react";
 
+import turtle1 from "../images/turtle1.svg";
+import turtle2 from "../images/turtle2.svg";
+
+import styled from "styled-components";
+import { Large } from "../components/typography";
+
+import { Header } from "../components/header";
+
+const SpinnerPage = styled.div`
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
+
+const SpinnerInner = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
 export default Component =>
   class WithLoadingSpinner extends React.Component {
+    state = {
+      frame: 0
+    };
+
+    componentDidMount() {
+      window.setInterval(() => {
+        this.setState(() => ({
+          frame: (this.state.frame + 1) % 2
+        }));
+      }, 500);
+    }
+
     render() {
       if (this.props.data && !this.props.data.loading) {
         return <Component {...this.props} />;
       } else {
-        return <div>Spinner...</div>;
+        return (
+          <React.Fragment>
+            <Header />
+            <SpinnerPage>
+              <SpinnerInner>
+                <img
+                  src={turtle1}
+                  style={{
+                    minHeight: "300px",
+                    display: this.state.frame === 1 ? "none" : "block"
+                  }}
+                  width="300px"
+                />
+                <img
+                  src={turtle2}
+                  style={{
+                    minHeight: "300px",
+                    display: this.state.frame === 0 ? "none" : "block"
+                  }}
+                  width="300px"
+                />
+                <Large>Loading...</Large>
+              </SpinnerInner>
+            </SpinnerPage>
+          </React.Fragment>
+        );
       }
     }
   };
