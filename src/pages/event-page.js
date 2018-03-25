57de56e421a266088456a94115e1bbc708e2438c
Stephen Bennett <sbennett@atlassian.com>
2018-03-24 17:38:57 -0700

    Start of video streaming

--- src/pages/event-page.js
+++ src/pages/event-page.js
@@ -5,10 +5,35 @@ import eventQuery from "./event.query.graphql";
 import EventCardList from "../components/event-card-list";
 import EventCard from "../components/event-card";
 import withLoadingSpinner from "../components/with-loading-spinner";
+import { OTSession, OTPublisher, OTStreams, OTSubscriber } from "opentok-react";
+import netlifyIdentity from "netlify-identity-widget";
 
 class EventPageView extends Component {
+  renderVideo() {
+    const { session } = this.props.data.event.session;
+    const currentUser = netlifyIdentity.currentUser();
+    console.log(currentUser);
+
+    return session && session.accessToken ? (
+      <OTSession
+        apiKey="46086982"
+        sessionId={session.id}
+        token={session.accessToken}
+      >
+        {session.stream === currentUser.id ? <OTPublisher /> : null}
+        <OTStreams>
+          <OTSubscriber />
+        </OTStreams>
+      </OTSession>
+    ) : null;
+  }
+
   render() {
-    return <PageLayout title={this.props.data.event.title} />;
+    return (
+      <PageLayout title={this.props.data.event.title}>
+        {this.renderVideo()}
+      </PageLayout>
+    );
   }
 }
 
