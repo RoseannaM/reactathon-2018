925f827f2efdc58d2cddd02e992ae5cac230b413
Stephen Bennett <sbennett@atlassian.com>
2018-03-24 17:47:15 -0700

    Fix merge conflicts

--- src/App.js
+++ src/App.js
@@ -15,54 +15,36 @@ import { theme } from "./theme.js";
 import { EventListPage } from "./pages/event-list-page";
 
 import netlifyIdentity from "netlify-identity-widget";
-import {loginUser, logoutUser} from "./identityActions";
+import { loginUser, logoutUser } from "./identityActions";
 
 class App extends Component {
-
-  state = {user: null}
+  state = { user: null };
 
   componentDidMount() {
     const user = localStorage.getItem("currentCoolUser");
 
     if (user) {
-      this.setState({user: JSON.parse(user)});
+      this.setState({ user: JSON.parse(user) });
     } else {
       loginUser();
     }
 
-    netlifyIdentity.on("login", (user) => this.setState({user}, loginUser()));
-    netlifyIdentity.on("logout", () => this.setState({user: null}, logoutUser()));
+    netlifyIdentity.on("login", user => this.setState({ user }, loginUser()));
+    netlifyIdentity.on("logout", () =>
+      this.setState({ user: null }, logoutUser())
+    );
   }
 
   handleLogIn = () => {
     netlifyIdentity.open();
-  }
+  };
 
   handleLogOut = () => {
     netlifyIdentity.logout();
-  }
+  };
 
   render() {
     return (
-<<<<<<< HEAD
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <div className="App">
-            <style>
-              @import
-              url('https://fonts.googleapis.com/css?family=IBM+Plex+Serif');
-            </style>
-            <Route path="/" exact component={Landing} />
-            <Route path="/page1" component={Page1} />
-            <Route path="/page2" component={Page2} />
-            <Route path="/event-start" component={EventStart} />
-            <Route path="/event" component={Event} />
-            <Route path="/login" component={Login} />
-            
-          </div>
-        </BrowserRouter>
-      </ApolloProvider>
-=======
       <MuiThemeProvider theme={theme}>
         <ApolloProvider client={client}>
           <BrowserRouter>
@@ -83,7 +65,6 @@ class App extends Component {
           </BrowserRouter>
         </ApolloProvider>
       </MuiThemeProvider>
->>>>>>> 85523f9bbd59307bbe6513be541217e8338d941d
     );
   }
 }
